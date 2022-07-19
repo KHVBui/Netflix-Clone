91a1ed2d69352a233197d4c0c6665868ef80e530
Kevin Bui <khvbui@gmail.com>
2022-07-18 22:17:05 -0700

    Style the formatting of the banner
    -Add CSS rules
    -Add hover effect for buttons
    -Add truncation to the banner description

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect } from 'react'
 import axios from './axios'
 import requests from './requests';
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -19,6 +20,10 @@ function Banner() {
     fetchData();
   }, []);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     // Banner image
     <header
@@ -33,17 +38,20 @@ function Banner() {
     >
       <div className="banner__contents"> {/* Text for banner */}
         {/* title */}
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         
         {/* div > 2 buttons */}
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>  
-
-        <h1 className="banner__description">{movie?.overview}</h1>
         
         {/* description */}
+        <h1 className="banner__description"x>
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div> 
       
     </header>
