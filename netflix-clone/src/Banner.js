7739de5d8238b8e6331f69f3413f8afdc09d82cc
Kevin Bui <khvbui@gmail.com>
2022-07-18 21:21:08 -0700

    Create the javascript for Banner
    -Picks random Netflix original to be the banner
    -Added title
    -Added two buttons: "Play", "My List"
    -Added movie overview

---
+++ netflix-clone/src/Banner.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect } from 'react'
+import axios from './axios'
+import requests from './requests';
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      
+      // Obtain one random Netflix original to be the banner
+      setMovie(  
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+    fetchData();
+  }, []);
+
+  return (
+    // Banner image
+    <header
+      className="banner" 
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+          "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+        )`,
+        backgroundPosition: "center center",
+      }} 
+    >
+      <div className="banner__contents"> {/* Text for banner */}
+        {/* title */}
+        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        
+        {/* div > 2 buttons */}
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>  
+
+        <h1 className="banner__description">{movie?.overview}</h1>
+        
+        {/* description */}
+      </div> 
+      
+    </header>
+  );
+}
+
+export default Banner
