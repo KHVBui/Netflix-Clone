0f6a367a64028476c437e0093b4b82b0f202a09b
Kevin Bui <khvbui@gmail.com>
2022-07-19 23:56:34 -0700

    Enable dns prefetch for Netflix logo
    -Speeds up loadtime
    -Adding `dns-prefetch` resource hints to establish early connections to important third-party origins

--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -19,6 +19,7 @@ function Nav() {
   
   return (
     <div className={`nav ${show && "nav__black"}`}>
+      <link rel="dns-prefetch" href="https://upload.wikimedia.org"></link>
       <img
         className="nav__logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
