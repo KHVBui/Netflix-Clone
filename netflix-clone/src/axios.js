1fef4971bebd0319a7a925b42b989f2bd3daf8fb
Kevin Bui <khvbui@gmail.com>
2022-08-07 02:32:55 -0700

    Add rate limiting and api retries
    Also provides console log message for retries

--- netflix-clone/src/axios.js
+++ netflix-clone/src/axios.js
@@ -1,8 +1,25 @@
- import axios from "axios";
+import axios from "axios";
+import rateLimit from "axios-rate-limit"
+import axiosRetry from 'axios-retry';
 
  /** base url to make requests to the movie database */
-const instance = axios.create({
+const instance = rateLimit(axios.create({
   baseURL: "https://api.themoviedb.org/3",
+  }), 
+  {
+    maxRequests: 50,
+    perMilliseconds: 1000
+  }
+);
+
+// Retries the API call if there are failed requests 
+axiosRetry(instance, { 
+  retries: 3, 
+  retryDelay: (retryCount) => {
+    console.log(`retry attempt: ${retryCount}`);
+    return retryCount * 2000;
+  },
+  retryCondition: axiosRetry.isRetryableError
 });
 
 export default instance; // default allows us to name it to anything in other files
