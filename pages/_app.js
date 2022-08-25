29b5cbcfce9d4358cc93820489b0bf4c6320ded3
Kevin Bui <khvbui@gmail.com>
2022-08-25 12:18:06 -0700

    Switch back contents of _app and index
    - Also adjust _app to be more like future pattern

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,7 @@
-import Head from "next/head";
-import Banner from "../src/Banner";
-import Nav from "../src/Nav";
-import requests from "../src/requests";
-import Row from "../src/Row";
 import "../styles/globals.css";
 
-function App() {
-	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
-			<Head>
-				<title>Home - Netflix</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<div className="app">
-				<Nav />
-				<Banner />
-				<Row title="TOP RATED" fetchUrl={requests.fetchTopRated} isLargeRow />
-				<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-				<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-				<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-				<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-				<Row title="Romance Movies " fetchUrl={requests.fetchRomanceMovies} />
-				<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-				<Row title="Family Movies" fetchUrl={requests.fetchFamilyMovies} />
-			</div>
-		</div>
-	);
+function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />; // eslint-disable-line react/jsx-props-no-spreading
 }
 
-export default App;
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./_app";
+import Head from "next/head";
+import Banner from "../src/Banner";
+import Nav from "../src/Nav";
+import requests from "../src/requests";
+import Row from "../src/Row";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-);
+function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Home - Netflix</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className="app">
+				<Nav />
+				<Banner />
+				<Row title="TOP RATED" fetchUrl={requests.fetchTopRated} isLargeRow />
+				<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+				<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+				<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+				<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+				<Row title="Romance Movies " fetchUrl={requests.fetchRomanceMovies} />
+				<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+				<Row title="Family Movies" fetchUrl={requests.fetchFamilyMovies} />
+			</div>
+		</div>
+	);
+}
+
+export default Home;
