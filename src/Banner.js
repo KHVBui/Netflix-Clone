9ee1fd3759bd803511ea7244614ff0712007da2f
Kevin Bui <khvbui@gmail.com>
2022-08-25 12:34:24 -0700

    Correctly link CSS components to js files

--- src/Banner.js
+++ src/Banner.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "../styles/Banner.module.css";
+import styles from "../styles/Banner.module.css";
 import axios from "./axios";
 import requests from "./requests";
 
@@ -31,7 +31,7 @@ function Banner() {
 	return (
 		// Banner image
 		<header
-			className="banner"
+			className={styles.banner}
 			style={{
 				backgroundSize: "cover",
 				backgroundImage: `url(
@@ -53,25 +53,25 @@ function Banner() {
 				}}
 			/>
 
-			<div className="banner__contents">
+			<div className={styles.banner__contents}>
 				{" "}
 				{/* Text for banner */}
 				{/* title */}
-				<h1 className="banner__title">
+				<h1 className={styles.banner__title}>
 					{movie?.title || movie?.name || movie?.original_name}
 				</h1>
 				{/* div > 2 buttons */}
-				<div className="banner__buttons">
-					<button className="banner__button">Play</button>
-					<button className="banner__button">My List</button>
+				<div className={styles.banner__buttons}>
+					<button className={styles.banner__button}>Play</button>
+					<button className={styles.banner__button}>My List</button>
 				</div>
 				{/* description */}
-				<h1 className="banner__description" x>
+				<h1 className={styles.banner__description} x>
 					{truncate(movie?.overview, 150)}
 				</h1>
 			</div>
 
-			<div className="banner--fadeBottom" />
+			<div className={styles["banner--fadeBottom"]} />
 		</header>
 	);
 }
