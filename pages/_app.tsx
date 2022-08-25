0e39b78479b243a37dbe0d8d2208ca02333c27f7
Kevin Bui <khvbui@gmail.com>
2022-08-25 00:01:35 -0700

    Correctly apply the App and index file patterns
    - At least, according to the old React app
    - May change in future upon learning proper NextJS conventions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,31 @@
+import Head from "next/head";
+import Banner from "../src/Banner";
+import Nav from "../src/Nav";
+import requests from "../src/requests";
+import Row from "../src/Row";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+function App() {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<Head>
+				<title>Home - Netflix</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className="app">
+				<Nav />
+				<Banner />
+				<Row title="TOP RATED" fetchUrl={requests.fetchTopRated} isLargeRow />
+				<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+				<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+				<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+				<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+				<Row title="Romance Movies " fetchUrl={requests.fetchRomanceMovies} />
+				<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+				<Row title="Family Movies" fetchUrl={requests.fetchFamilyMovies} />
+			</div>
+		</div>
+	);
 }
 
-export default MyApp;
+export default App;
