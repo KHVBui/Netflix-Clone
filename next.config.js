01cd20f2214051bff7d5870a39f5336f003be730
Kevin Bui <khvbui@gmail.com>
2022-09-03 01:51:17 -0700

    Fixed problems with Image styling
    - Implemented next/future/image for banner and nav to enforce normal CSS styling
    - img styling in globals removed bc/ forced row posters to fit in one viewport
    - Added domains for avatar and Netflix logo
    - Row is able to use normal Next Images due to using layout="fixed"
    - className moved to wrapper div in order for margins to work correctly

--- next.config.js
+++ next.config.js
@@ -2,8 +2,16 @@
  * @type {import('next').NextConfig}
  */
 module.exports = {
+	experimental: {
+		images: {
+			allowFutureImage: true
+		}
+	},
 	reactStrictMode: true,
 	images: {
-		domains: ["image.tmdb.org"]
-	}
+		domains: [
+			"image.tmdb.org",
+			"upload.wikimedia.org",
+			"mir-s3-cdn-cf.behance.net"
+	]}
 };
