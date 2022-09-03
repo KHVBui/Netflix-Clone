56b409a40fec908f9f4325f76f2934bcba79907a
Kevin Bui <khvbui@gmail.com>
2022-09-03 01:51:17 -0700

    Fixed problems with Image styling
    - Implemented next/future/image for banner and nav to enforce normal CSS styling
    - img styling in globals removed bc/ forced row posters to fit in one viewport
    - Added domains for avatar and Netflix logo
    - Row is able to use normal Next Images due to using layout="fixed"
    - className moved to wrapper div in order for margins to work correctly

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/future/image";
 import { useEffect, useState } from "react";
 import styles from "../styles/Nav.module.css";
 
@@ -20,16 +20,22 @@ function Nav() {
 	return (
 		<div className={`${styles.nav} ${show && styles.nav__black}`}>
 			<link rel="dns-prefetch" href="https://upload.wikimedia.org" />
-			<Image
-				className={styles.nav__logo}
-				src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
-				alt="Netflix Logo"
-			/>
-			<Image
-				className={styles.nav__avatar}
-				src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
-				alt="Netflix Logo"
-			/>
+			<div className={styles.nav__logo}>
+				<Image
+					src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+					alt="Netflix Logo"
+					width="100"
+					height="27"
+				/>
+			</div>
+			<div className={styles.nav__avatar}>
+				<Image
+					src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+					alt="Netflix Logo"
+					width="30"
+					height="30"
+				/>
+			</div>
 			<div className={styles["nav--fadeTop"]} />
 		</div>
 	);
