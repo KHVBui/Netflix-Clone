c995534fc48ca694b091221e1c9a7274b2107a6e
Kevin Bui <khvbui@gmail.com>
2022-09-03 23:57:48 -0700

    Apply prettier formatting for header styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,8 @@ import Header from "../components/Header";
 
 const Home: NextPage = () => {
 	return (
-		<div className="relative h-screen bg-gradient-to-b from-gray-900/10
+		<div
+			className="relative h-screen bg-gradient-to-b from-gray-900/10
 			to-[#010511] lg:h-[140vh]"
 		>
 			<Head>
