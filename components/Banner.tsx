d38433c54ed6f83e4eb61ef335006eed521ffe9e
Kevin Bui <khvbui@gmail.com>
2022-09-06 17:30:16 -0700

    refactor: Row now uses Typescript and Tailwind (#30)
    - Row uses Thumbnail component to render movie photos now
    - feature change: Arrows present on carousel for fast scrolling
    - feature change: vertical scrollbar is red
    - Banner formatting fixed to cover screen and fade to black
       - gradient styling configured in tailwind.config.js
    - Removed raw API key from requests.ts
    
    - New dependencies:
      - tailwind-scrollbar-hide
         to hide scrollbar
      - tailwind-scrollbar
        to implement nicer looking scrollbar
      - prettier-plugin-tailwindcss
         for automatic tailwind reformatting
    - no longer using axios or movie-trailer
    - web-vitals also removed
    - Unnecessary CSS modules now removed

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -25,7 +25,7 @@ function Banner({ netflixOriginals }: Props) {
 			lg:h-[65vh] lg:justify-end lg:pb-12"
 		>
 			{/* Banner image */}
-			<div className="absolute top=0 left-0 -z-10 h-[95vh] w-screen">
+			<div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
 				<Image
 					src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
 					alt="banner"
--- utils/requests.ts
+++ utils/requests.ts
@@ -1,4 +1,4 @@
-const API_KEY = "54613b0ade03781754a212ba7b020c53";
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 const requests = {
