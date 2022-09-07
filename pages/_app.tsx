fafa1986573cd6f0c133e347e54217940edbb711
Kevin Bui <khvbui@gmail.com>
2022-09-07 15:23:41 -0700

    feat: Login Page and Firebase Auth (#33)
    - functional login screen was added
    - login uses custom hook useAuth
      - customError state currently used as a filler
      - Saves accounts that are signed up and only allows them to login
      - memoization used to save states and only change one of the states upon a change
    - Homepage (index + Header) can logout and show a blank screen if loading
    - The app is now wrapped by the AuthProvider component
    
    - "rb.gy" added to next.config.js image domains
    - globals styles was cleaned up
    
    - Added dependencies:
      - firebase (for authentication, not hosting)
      - react-hook-form used for the form handling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from "next/app";
+import { AuthProvider } from "../hooks/useAuth";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />; // eslint-disable-line react/jsx-props-no-spreading
+	return (
+		// Higher order component
+		<AuthProvider>
+			<Component {...pageProps} />;
+		</AuthProvider>
+	);
 }
 
 export default MyApp;
