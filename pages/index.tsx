643adb5a2962726559075ded690586595b4e944b
Kevin Bui <khvbui@gmail.com>
2022-09-06 17:30:16 -0700

    refactor: Row now uses Typescript and Tailwind (#30)
    - Row uses Thumbnail component to render movie photos now
    - feature change: Arrows present on carousel for fast scrolling
    - feature change: vertical scrollbar is red
    - Banner formatting fixed to cover screen and fade to black
       - gradient styling configured in tailwind.config.js
    - Removed raw API key from requests.ts
    
    - New dependencies:
      - tailwind-scrollbar-hide
         to hide scrollbar
      - tailwind-scrollbar
        to implement nicer looking scrollbar
      - prettier-plugin-tailwindcss
         for automatic tailwind reformatting
    - no longer using axios or movie-trailer
    - web-vitals also removed
    - Unnecessary CSS modules now removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Row from "../components/Row";
 import { Movie } from "../types";
 import requests from "../utils/requests";
-// import Row from "../src/Row";
 
 interface Props {
 	netflixOriginals: Movie[];
@@ -27,25 +27,26 @@ const Home = ({
 }: Props) => {
 	return (
 		<div
-			className="relative h-screen bg-gradient-to-b from-gray-900/10
-			to-[#010511] lg:h-[140vh]"
+			className="relative h-screen bg-gradient-to-b
+			from-gray-900/10 to-[#010511] lg:h-[140vh]"
 		>
 			<Head>
 				<title>Home - Netflix</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Header />
-			<main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+			<main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
 				<Banner netflixOriginals={netflixOriginals} />
-				{/* <link rel="dns-prefetch" href="https://api.themoviedb.org" />
-				<Row title="TOP RATED" fetchUrl={requests.fetchTopRated} isLargeRow />
-				<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-				<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-				<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-				<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-				<Row title="Romance Movies " fetchUrl={requests.fetchRomanceMovies} />
-				<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-				<Row title="Family Movies" fetchUrl={requests.fetchFamilyMovies} /> */}
+
+				<section className="md:space-y-24">
+					<Row title="Trending Now" movies={trendingNow} />
+					<Row title="Top Rated" movies={topRated} />
+					<Row title="Action Thrillers" movies={actionMovies} />
+					<Row title="Comedies" movies={comedyMovies} />
+					<Row title="Scary Movies" movies={horrorMovies} />
+					<Row title="Romance Movies" movies={romanceMovies} />
+					<Row title="Documentaries" movies={documentaries} />
+				</section>
 			</main>
 		</div>
 	);
