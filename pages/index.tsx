71a61b6495fff4c3db1b1a793fd211bc4f80a2fa
Kevin Bui <khvbui@gmail.com>
2022-09-22 14:12:47 -0700

    feat: Added subscription page for new users (#36)
    - Unsubscribed users can no longer see the movies. Their landing page upon login is the subscription page.
    - Subscription page encompassed in "Plans", with sub components Table with the plan details, and Loader for the loading circle when the user clicks on the subscribe button.
    - useSubscription hook created to check if user is subscribed through Stripe and Firebase
    - lib/stripe.ts added to handle redirect to stripe payment page
    - File next.config needs to transpile the stripe firestore payment package. Also added additional image source.
    
    - packages added: firebase functions, stripe, next-transpile-modules, encoding (to resolve a node package error)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
+import { getProducts, Product } from "@stripe/firestore-stripe-payments";
 import Head from "next/head";
 import { useRecoilValue } from "recoil";
-import { modalState } from "../atoms/modalAtom";
+import { modalState, movieState } from "../atoms/modalAtom";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import Modal from "../components/Modal";
+import Plans from "../components/Plans";
 import Row from "../components/Row";
 import useAuth from "../hooks/useAuth";
+import useSubscription from "../hooks/useSubscription";
+import payments from "../lib/stripe";
 import { Movie } from "../types";
 import requests from "../utils/requests";
 
@@ -18,6 +22,7 @@ interface Props {
 	horrorMovies: Movie[];
 	romanceMovies: Movie[];
 	documentaries: Movie[];
+	products: Product[];
 }
 const Home = ({
 	netflixOriginals,
@@ -28,11 +33,16 @@ const Home = ({
 	romanceMovies,
 	topRated,
 	trendingNow,
+	products,
 }: Props) => {
-	const { loading } = useAuth();
+	const { loading, user } = useAuth();
 	const showModal = useRecoilValue(modalState);
+	const subscription = useSubscription(user);
+	const movie = useRecoilValue(movieState);
 
-	if (loading) return null;
+	if (loading || subscription === null) return null;
+
+	if (!subscription) return <Plans products={products} />;
 
 	return (
 		<div
@@ -66,6 +76,13 @@ export default Home;
 
 // Use server-side rendering
 export const getServerSideProps = async () => {
+	const products = await getProducts(payments, {
+		includePrices: true,
+		activeOnly: true,
+	})
+		.then(res => res)
+		.catch(error => console.log(error.message));
+
 	const [
 		netflixOriginals,
 		trendingNow,
@@ -96,6 +113,7 @@ export const getServerSideProps = async () => {
 			horrorMovies: horrorMovies.results,
 			romanceMovies: romanceMovies.results,
 			documentaries: documentaries.results,
+			products,
 		},
 	};
 };
