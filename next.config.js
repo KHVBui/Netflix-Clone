b86ddbd6b1e0247ecac3bb8cbcd44843828bfc82
Kevin Bui <khvbui@gmail.com>
2022-09-22 14:12:47 -0700

    feat: Added subscription page for new users (#36)
    - Unsubscribed users can no longer see the movies. Their landing page upon login is the subscription page.
    - Subscription page encompassed in "Plans", with sub components Table with the plan details, and Loader for the loading circle when the user clicks on the subscribe button.
    - useSubscription hook created to check if user is subscribed through Stripe and Firebase
    - lib/stripe.ts added to handle redirect to stripe payment page
    - File next.config needs to transpile the stripe firestore payment package. Also added additional image source.
    
    - packages added: firebase functions, stripe, next-transpile-modules, encoding (to resolve a node package error)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const withTM = require('next-transpile-modules')([
+  '@stripe/firestore-stripe-payments',
+]) // pass the modules you would like to see transpiled
+
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = {
+module.exports = withTM({
 	experimental: {
 		images: {
 			allowFutureImage: true
@@ -13,8 +17,9 @@ module.exports = {
 			"image.tmdb.org",
 			"upload.wikimedia.org",
 			"mir-s3-cdn-cf.behance.net",
-			"rb.gy"
+			"rb.gy",
+			"assets.nflxext.com"
 		],
 		deviceSizes: [30, 100, 170, 180, 1200]
 	}
-};
+});
